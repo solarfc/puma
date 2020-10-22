3d08a140908d6e9061b89c8fa12603a3e7c0c497
vladyslav1243 <galich.vinfort@gmail.com>
2020-10-22 17:50:55 +0300

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -5,29 +5,159 @@ var myWidth = window.innerWidth,
 console.log("width ".concat(myWidth, " \n height ").concat(myHeight));
 
 window.onload = function () {
-  var i = 1;
-  var obj = {
-    'Пункт 1': [1, 2, 3],
-    'Пункт 2': [4, 5, 6]
-  };
-  console.log(obj[1]);
-  $.each(obj, function (key) {
-    $('#main-select').append("<option value=\"".concat(key, "\">").concat(key, "</option>"));
+  /*
+      increase date
+   */
+  var today = new Date(),
+      tomorrow = new Date(),
+      day,
+      month,
+      year,
+      i = 3,
+      period = document.querySelectorAll('.to-order p output');
+  tomorrow.setDate(today.getDate() + i);
+  day = tomorrow.getDate() > 9 ? tomorrow.getDate() : "0".concat(tomorrow.getDate());
+  month = tomorrow.getMonth() + 1 > 9 ? tomorrow.getMonth() + 1 : "0".concat(tomorrow.getMonth() + 1);
+  year = tomorrow.getFullYear();
+
+  for (var _i = 0; _i < period.length; _i++) {
+    period[_i].innerHTML = "".concat(day, ".").concat(month, ".").concat(year.toString().slice(2));
+  }
+
+  document.querySelector('.footer p.small.address output').innerHTML = year;
+  /*
+      loop fancybox
+   */
+
+  $.fancybox.defaults.loop = true;
+  /*
+      form styler
+   */
+
+  $(function () {
+    $('select').styler({
+      selectSmartPositioning: false
+    });
   });
-  $('#main-select').on('change', function () {
-    var a = $('#main-select option:selected').val();
-    $('body').append("<select name=\"".concat(a, "\" id=\"").concat(i++, "\"><option>").concat($('#main-select option:selected').val(), "</option></select>"));
-    console.log(a);
-    console.log(obj[a]);
-    $('#main-select option:selected').addClass('hide');
+  /*
+      change color and photo
+   */
 
-    for (var _i = 0; _i < obj[a].length; _i++) {
-      $("select[name=\"".concat(a, "\"]")).append("<option>".concat(obj[a][_i], "</option>"));
-    } // for(let key in obj) {
-    // }
+  var manColor = document.querySelectorAll('.catalog__block.man .color figure span'),
+      womanColor = document.querySelectorAll('.catalog__block.woman .color figure span'),
+      bigManBoot = document.querySelector('.catalog__block.man .catalog__block-img img.big'),
+      bigWomanBoot = document.querySelector('.catalog__block.woman .catalog__block-img img.big'),
+      smallManBoot = document.querySelectorAll('.catalog__block.man .catalog__block-img a img'),
+      smallManLink = document.querySelectorAll('.catalog__block.man .catalog__block-img a'),
+      smallWomanBoot = document.querySelectorAll('.catalog__block.woman .catalog__block-img a img'),
+      smallWomanLink = document.querySelectorAll('.catalog__block.woman .catalog__block-img a'),
+      changeColor = function changeColor(color, img, photo, link) {
+    var _loop = function _loop(_i2) {
+      var _loop2 = function _loop2(j) {
+        color[j].addEventListener('click', function () {
+          if (color[j].classList.contains('active')) {
+            color[_i2].classList.remove('active');
 
-  }); // console.log(obj);
-  // /*
+            color[j].classList.add('active');
+          } else {
+            color[_i2].classList.remove('active');
+
+            color[j].classList.add('active');
+            var activeColor = color[j].classList.value;
+            var activeModel = color[j].dataset.model;
+
+            var hide = function hide() {
+              img.style.opacity = '0';
+
+              for (var _i3 = 0; _i3 < photo.length; _i3++) {
+                photo[_i3].style.opacity = '0';
+                link[_i3].style.opacity = '0';
+              }
+            };
+
+            var change = function change() {
+              img.src = "img/catalog/".concat(activeModel, "/big.png");
+              img.className = "big ".concat(activeColor);
+
+              for (var _i4 = 0; _i4 < photo.length; _i4++) {
+                photo[_i4].src = "img/catalog/".concat(activeModel, "/").concat(_i4 + 1, "s.jpg");
+                link[_i4].href = "img/catalog/".concat(activeModel, "/").concat(_i4 + 1, ".jpg");
+              }
+            };
+
+            var show = function show() {
+              img.style.opacity = '1';
+
+              for (var _i5 = 0; _i5 < photo.length; _i5++) {
+                photo[_i5].style.opacity = '1';
+                link[_i5].style.opacity = '1';
+              }
+            };
+
+            hide();
+            setTimeout(change, 750);
+            setTimeout(show, 1000);
+          }
+        });
+      };
+
+      for (var j = 0; j < color.length; j++) {
+        _loop2(j);
+      }
+    };
+
+    for (var _i2 = 0; _i2 < color.length; _i2++) {
+      _loop(_i2);
+    }
+  };
+
+  changeColor(manColor, bigManBoot, smallManBoot, smallManLink);
+  changeColor(womanColor, bigWomanBoot, smallWomanBoot, smallWomanLink);
+  /*
+      review slider
+   */
+
+  $('.review__content-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rows: 0,
+    speed: 300,
+    arrows: true,
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow')
+  });
+  /*
+      parallax
+   */
+
+  var advantagesVector = document.querySelector('.advantages__content-img .vector'),
+      womanLeftVector = document.querySelector('.catalog__block.woman .vector.left'),
+      womanRightVector = document.querySelector('.catalog__block.woman .vector.right'),
+      manLeftVector = document.querySelector('.catalog__block.man .vector.left'),
+      top,
+      left,
+      move = function move(top, left) {
+    advantagesVector.style.transform = "translate3d(".concat(left, ", ").concat(top, ", ").concat(top, ")");
+    womanLeftVector.style.transform = "translate3d(".concat(left, ", ").concat(top, ", ").concat(top, ")");
+    womanRightVector.style.transform = "translate3d(-".concat(left, ", -").concat(top, ", -").concat(top, ")");
+    manLeftVector.style.transform = "translate3d(-".concat(left, ", ").concat(top, ", -").concat(top, ")");
+  },
+      setupForWidth = function setupForWidth(mql) {
+    if (mql.matches) {
+      document.addEventListener('mousemove', function () {
+        top = "".concat(event.clientY * 10 / myHeight, "px");
+        left = "".concat(event.clientX * 10 / myWidth, "px");
+        move(top, left);
+      });
+    } else {
+      top = 0;
+      left = 0;
+    }
+  },
+      mql = window.matchMedia('screen and (min-width: 1200px)');
+
+  mql.addListener(setupForWidth);
+  setupForWidth(mql); // /*
   //     change href on mobile
   //  */
   //
--- app/script/main.js
+++ app/script/main.js
@@ -4,37 +4,150 @@ console.log(`width ${myWidth} \n height ${myHeight}`);
 
 window.onload = function () {
 
-    let i = 1;
+    /*
+        increase date
+     */
 
-    const obj = {
-        'Пункт 1': [1, 2, 3],
-        'Пункт 2': [4, 5, 6]
-    };
+    let today = new Date(),
+        tomorrow = new Date(),
+        day,
+        month,
+        year,
+        i = 3,
+        period = document.querySelectorAll('.to-order p output');
 
-    console.log(obj[1]);
+    tomorrow.setDate(today.getDate() + i);
+    day = (tomorrow.getDate() > 9) ? tomorrow.getDate() : `0${tomorrow.getDate()}`;
+    month = (tomorrow.getMonth() + 1 > 9) ? tomorrow.getMonth() + 1 : `0${tomorrow.getMonth() + 1}`;
+    year = tomorrow.getFullYear();
 
-    $.each(obj, function (key) {
-        $('#main-select').append(`<option value="${key}">${key}</option>`)
+    for(let i = 0; i < period.length; i++) {
+        period[i].innerHTML = `${day}.${month}.${year.toString().slice(2)}`;
+    }
+
+    document.querySelector('.footer p.small.address output').innerHTML = year;
+
+    /*
+        loop fancybox
+     */
+
+    $.fancybox.defaults.loop = true;
+
+    /*
+        form styler
+     */
+
+    $(function () {
+        $('select').styler({
+            selectSmartPositioning: false
+        });
     });
 
-    $('#main-select').on('change', function () {
-        let a = $('#main-select option:selected').val();
-        $('body').append(`<select name="${a}" id="${i++}"><option>${$('#main-select option:selected').val()}</option></select>`);
-        console.log(a);
-        console.log(obj[a]);
-        $('#main-select option:selected').addClass('hide');
+    /*
+        change color and photo
+     */
 
-        for(let i = 0; i<obj[a].length; i++) {
-            $(`select[name="${a}"]`).append(`<option>${obj[a][i]}</option>`)
-        }
+    const manColor = document.querySelectorAll('.catalog__block.man .color figure span'),
+        womanColor = document.querySelectorAll('.catalog__block.woman .color figure span'),
+        bigManBoot = document.querySelector('.catalog__block.man .catalog__block-img img.big'),
+        bigWomanBoot = document.querySelector('.catalog__block.woman .catalog__block-img img.big'),
+        smallManBoot = document.querySelectorAll('.catalog__block.man .catalog__block-img a img'),
+        smallManLink = document.querySelectorAll('.catalog__block.man .catalog__block-img a'),
+        smallWomanBoot = document.querySelectorAll('.catalog__block.woman .catalog__block-img a img'),
+        smallWomanLink = document.querySelectorAll('.catalog__block.woman .catalog__block-img a'),
+        changeColor = (color, img, photo, link) => {
+            for(let i = 0; i < color.length; i++) {
+                for(let j = 0; j < color.length; j++) {
+                    color[j].addEventListener('click', () => {
+                        if(color[j].classList.contains('active')) {
+                            color[i].classList.remove('active');
+                            color[j].classList.add('active');
+                        } else {
+                            color[i].classList.remove('active');
+                            color[j].classList.add('active');
+                            let activeColor = color[j].classList.value;
+                            let activeModel = color[j].dataset.model;
+                            const hide = () => {
+                                img.style.opacity = '0';
+                                for(let i = 0; i < photo.length; i++) {
+                                    photo[i].style.opacity = '0';
+                                    link[i].style.opacity = '0';
+                                }
+                            }
+                            const change = () => {
+                                img.src = `img/catalog/${activeModel}/big.png`;
+                                img.className = `big ${activeColor}`;
+                                for(let i = 0; i < photo.length; i++) {
+                                    photo[i].src = `img/catalog/${activeModel}/${i + 1}s.jpg`;
+                                    link[i].href = `img/catalog/${activeModel}/${i + 1}.jpg`;
+                                }
+                            }
+                            const show = () => {
+                                img.style.opacity = '1';
+                                for(let i = 0; i < photo.length; i++) {
+                                    photo[i].style.opacity = '1';
+                                    link[i].style.opacity = '1';
+                                }
+                            }
+                            hide();
+                            setTimeout(change, 750);
+                            setTimeout(show, 1000);
+                        }
+                    });
+                }
+            }
+        };
 
-        // for(let key in obj) {
-        // }
+    changeColor(manColor, bigManBoot, smallManBoot, smallManLink);
+    changeColor(womanColor, bigWomanBoot, smallWomanBoot, smallWomanLink);
+
+    /*
+        review slider
+     */
+
+    $('.review__content-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 0,
+        speed: 300,
+        arrows: true,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.next-arrow')
     });
 
+    /*
+        parallax
+     */
 
+    let advantagesVector = document.querySelector('.advantages__content-img .vector'),
+        womanLeftVector = document.querySelector('.catalog__block.woman .vector.left'),
+        womanRightVector = document.querySelector('.catalog__block.woman .vector.right'),
+        manLeftVector = document.querySelector('.catalog__block.man .vector.left'),
+        top,
+        left,
+        move = (top, left) => {
+            advantagesVector.style.transform = `translate3d(${left}, ${top}, ${top})`;
+            womanLeftVector.style.transform = `translate3d(${left}, ${top}, ${top})`;
+            womanRightVector.style.transform = `translate3d(-${left}, -${top}, -${top})`;
+            manLeftVector.style.transform = `translate3d(-${left}, ${top}, -${top})`;
+        },
+        setupForWidth = (mql) => {
+            if(mql.matches) {
+                document.addEventListener('mousemove', () => {
+                    top = `${event.clientY * 10 / myHeight}px`;
+                    left = `${event.clientX * 10 / myWidth}px`;
+                    move(top, left);
+                });
+            } else {
+                top = 0;
+                left = 0;
+            }
+        },
+        mql = window.matchMedia('screen and (min-width: 1200px)');
 
-    // console.log(obj);
+    mql.addListener(setupForWidth);
+    setupForWidth(mql);
+
 
 
     // /*
